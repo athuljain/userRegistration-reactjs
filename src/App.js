8539c1d60b728490103e11fff70786c319a304ba
athuljain <athuljainkj2@gmail.com>
2023-10-10 16:06:13 +0530

    register commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import './App.css';
 import Register from './Component/Register';
 
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { myContext } from "./Component/Context";
+import { useState } from 'react';
 
 function App() {
+
+  const [user, setUser] = useState([]);
+
+  const values={
+    user, setUser
+  }
+
+  // console.log(user);
+
+
   return (
     <div className="App">
   
 <BrowserRouter>
+<myContext.Provider value={values}>
     <Routes>
       <Route path='/' element={<Register />} />
     </Routes>
+    </myContext.Provider>
 </BrowserRouter>
 
     </div>
--- src/Component/Register.jsx
+++ src/Component/Register.jsx
@@ -1,7 +1,58 @@
+import { useContext, useState } from "react"
+import { myContext } from "./Context"
+
 export default function Register(){
+
+    const {user, setUser} = useContext(myContext)
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleButtonClick = () => {
+        const userData = {  name,email,password,
+        };
+        setUser([...user,userData]  );
+       
+        // navigate("/login");
+        console.log("hlooo",user);
+      };
+      
+
     return(
         <div>
             <h1>Register </h1>
+            <div className="register-body">
+        <input
+          
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+       
+        <input
+          
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button  onClick={handleButtonClick} type="submit">
+          Submit 
+        </button>
+      </div>
         </div>
     )
 }
